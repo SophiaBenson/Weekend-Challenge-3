ae4cdb28f65a20e8f365d9917b1ad15df1d2fb67
Sophia Benson <sophiabenson@Sophias-MacBook-Pro.local>
2016-06-13 13:31:28 -0500

    finished style

--- modules/calc.js
+++ modules/calc.js
@@ -18,7 +18,7 @@ var calculatePath = function (var1, var2, var3) {
     return(div);
   }
   else{
-    return("bionic bird is sad");
+    return("Bionic Bird is sad");
   }
   // return("Bionic Bird is Calculating: " + inputX + operation + inputY);
 };
